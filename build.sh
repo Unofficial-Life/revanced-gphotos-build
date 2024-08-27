124abd5e47a389f09da92fc8ef8711a20635ce56
Unofficial Life <118039878+Unofficial-Life@users.noreply.github.com>
2024-08-27 20:50:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -178,9 +178,8 @@ wait
 rm -rf temp/tmp.*
 if [ -z "$(ls -A1 ${BUILD_DIR})" ]; then abort "All builds failed."; fi
 
-log "\nInstall [Microg](https://github.com/ReVanced/GmsCore/releases) for non-root YouTube and YT Music APKs"
-log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach root ReVanced YouTube and YT Music from Play Store"
-log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)\n"
+log "\nInstall [Microg](https://github.com/ReVanced/GmsCore/releases) for Revanced Google Photos to work"
+log "\n[revanced-magisk-module](https://github.com/Unofficial-Life/revanced-gphotos-build)\n"
 log "$(cat $TEMP_DIR/*-rv/changelog.md)"
 
 SKIPPED=$(cat $TEMP_DIR/skipped 2>/dev/null || :)
